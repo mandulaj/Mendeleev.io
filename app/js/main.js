de05787bd89ff7b49b7237ccd5adedb2eff8a626
Alexander Selzer <aselzer3@gmail.com>
2014-01-29 23:14:02 +0100

    Explained hash-based routing to *some* people.

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
 
+/**
+* Whenever the URL hash changes, which means that the
+* active "page" changes, changes, Mendeleev.showPage()
+* gets called to render the correct page.
+* It takes the responsibility of clearing the parent element.
+*/
+
 window.onhashchange = function() {
   Mendeleev.showPage(window.location.hash);
 }
 
-// "Redirect" to the default "page" - the periodic table.
+/**
+* If the URL hash is not set, for example in a browser
+* that just visited the page, it will get set to the
+* default page, which is the periodic table.
+* This triggers a hashchange event and the page is
+* updated.
+*/
 if (!window.location.hash) {
   window.location.hash = "periodic-table";
 }
 else {
-  // Trigger hashchange event.
+/**
+* Otherwise, if the user made a full request to the
+* server with the intention of visiting a specific pagem
+* the page specified in the hash is immediately served,
+* as it is not possible to fire a hashchange event.
+*/
   Mendeleev.showPage(window.location.hash);
 }
 
