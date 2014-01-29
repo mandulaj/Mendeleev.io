f12d0d39ae9622b0789d48cd1ac81cc6cbee833b
Jakub Mandula <jakub.aludnam@gmail.com>
2014-01-29 21:42:28 +0100

    chemistry math added
    objects and functions for handling chemistry operations

---
+++ app/chemistrymath.js
@@ -0,0 +1,22 @@
+var elements = require('elements.json');
+
+
+
+function Molecule()
+{
+	
+}
+
+
+function Expression(expression) 
+{
+	this.expression = expression;
+}
+
+
+function Equation ()
+{
+	this.leftHandSide = new Expression(left_expression);
+	this.rightHandSide = new Expression(right_expression);
+
+}
