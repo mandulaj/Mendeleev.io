f49b23db6e5cf81fbe30bd3220394457645d8d76
Jakub Mandula <jakub.aludnam@gmail.com>
2014-02-02 22:05:34 +0000

    setup chemistryjs, fix copernicium

--- app/chemistrymath.js
+++ app/chemistrymath.js
@@ -1,12 +1,19 @@
-var elements = require('elements.json');
+//Mendeleev.getElements(function(e){
+//	var elements = e;
+//});
+var elements = require("./elements.json")
 
 
-
-function Molecule()
+function Molecule(moles,equation)
 {
-	
+	this.n_moles = moles;
+	this.equation = equation;
 }
 
+Molecule.prototype.numOfElement = function(element)
+{
+	return this.equation.element
+}
 
 function Expression(expression) 
 {
@@ -20,3 +27,8 @@ function Equation ()
 	this.rightHandSide = new Expression(right_expression);
 
 }
+
+var test = new Molecule(1,{"C":2,"H":6})
+console.log(test.n_moles)
+console.log(test.equation)
+console.log(test.numOfElement(C))
